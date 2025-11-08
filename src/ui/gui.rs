8536dfd8f26b6207357d28e219b16f1d608d3d6d
Hiromitsu Iwata <hiromitsu7@gmail.com>
2025-11-08 18:10:22 +0900

    スペクトログラムGUI

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -1,6 +1,5 @@
 use std::sync::{Arc, Mutex};
-use eframe::egui::{self, Color32, ColorImage, TextureHandle};
-use egui::load::SizedTexture;
+use eframe::egui::{self, Color32, TextureHandle};
 
 /// GUIモードでのターミナル描画を行う
 pub fn run_ui(sample_rate: f32, spectrogram: Arc<Mutex<Vec<Vec<f32>>>>) -> anyhow::Result<()> {
@@ -23,16 +22,33 @@ struct SpectrogramApp {
 
 impl eframe::App for SpectrogramApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(format!("Sample rate: {}", self.sample_rate));
+        let spec = self.spectrogram.lock().unwrap().clone();
+        let width = spec.len();
+        let height = spec[0].len();
 
-            if let Some(texture) = &self.texture {
-                let texture2 = SizedTexture::new(texture, ui.available_size());
-                ui.image(texture2);
-            } else {
-                ui.label("Spectrogram texture not created yet");
+        // 黒で初期化
+        let pixels = vec![Color32::BLACK; width * height];
+        let mut image = egui::ColorImage::new([width, height], pixels);
+
+        for x in 0..width {
+            for y in 0..height {
+                let val = spec[x][y].clamp(0.0, 2.0);
+                let intensity = ((val / 2.0) * 255.0) as u8;
+                image[(x, height - 1 - y)] = egui::Color32::from_rgb(intensity, intensity / 2, 0);
             }
+        }
+
+        let texture = ctx.load_texture(
+            "spectrogram",
+            image,
+            egui::TextureOptions::NEAREST,
+        );
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.image((texture.id(), ui.available_size()));
         });
+
+        ctx.request_repaint(); // 常に更新
     }
 }
 
