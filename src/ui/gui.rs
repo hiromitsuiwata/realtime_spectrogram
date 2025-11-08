eb0f073759d98ed14ed1666b59aba909035ac025
Hiromitsu Iwata <hiromitsu7@gmail.com>
2025-11-08 18:01:37 +0900

    eframe::App

--- src/ui/gui.rs
+++ src/ui/gui.rs
@@ -1,30 +1,47 @@
 use std::sync::{Arc, Mutex};
-use eframe::egui;
+use eframe::egui::{self, Color32, ColorImage, TextureHandle};
+use egui::load::SizedTexture;
 
 /// GUIモードでのターミナル描画を行う
 pub fn run_ui(sample_rate: f32, spectrogram: Arc<Mutex<Vec<Vec<f32>>>>) -> anyhow::Result<()> {
     println!("GUIモードで起動します。");
     let options = eframe::NativeOptions::default();
 
-
-    // Our application state:
-    let mut name = "Arthur".to_owned();
-    let mut age = 42;
-
-    let _ = eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                age += 1;
-            }
-            ui.label(format!("Hello '{name}', age {age}"));
-        });
-    });
+    let _ = eframe::run_native(
+        "Spectrogram Viewer",
+        options,
+        Box::new(|_cc| Ok(Box::new(SpectrogramApp::new(sample_rate, spectrogram)))),
+    );
     Ok(())
+}
+
+struct SpectrogramApp {
+    sample_rate: f32,
+    spectrogram: Arc<Mutex<Vec<Vec<f32>>>>,
+    texture: Option<TextureHandle>,
+}
+
+impl eframe::App for SpectrogramApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(format!("Sample rate: {}", self.sample_rate));
+
+            if let Some(texture) = &self.texture {
+                let texture2 = SizedTexture::new(texture, ui.available_size());
+                ui.image(texture2);
+            } else {
+                ui.label("Spectrogram texture not created yet");
+            }
+        });
+    }
+}
+
+impl SpectrogramApp {
+    fn new(sample_rate: f32, spectrogram: Arc<Mutex<Vec<Vec<f32>>>>) -> Self {
+        Self {
+            sample_rate: sample_rate,
+            spectrogram: spectrogram,
+            texture: None,
+        }
+    }
 }
